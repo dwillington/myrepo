45996aac992ac63b9c675738870efa243ac0efa0
Amjad Ashraf <aashraf@scglobal.ad.scotiacapital.com>
2015-11-26 11:35:32 -0500

    mods

--- devops/job-dsl-scripts/maven-jobs.groovy
+++ devops/job-dsl-scripts/maven-jobs.groovy
@@ -1,20 +1,20 @@
 job('maven-sample') {
     scm 
-	{
-	  branch('develop')
-      git('https://github.com/dwillington/myrepo.git')
+    {
+        branch('develop')
+        git('https://github.com/dwillington/myrepo.git')
     }
     triggers 
-	{
+    {
         scm('*/15 * * * *')
     }
     steps 
-	{
+    {
         maven
 		{
-		  goals('-e clean package')
-          localRepository(LocalRepositoryLocation.LOCAL_TO_WORKSPACE)
- 		  rootPom('maven-projects/sample/pom.xml')
-		}
+            goals('-e clean package')
+            localRepository(LocalRepositoryLocation.LOCAL_TO_WORKSPACE)
+            rootPom('maven-projects/sample/pom.xml')
+        }
     }
 }
