4c3a9f9e86629662d2352c2b6eb087c4f41abeba
Ashraf <AMJAD_ASHRAF@homedepot.com>
2016-12-13 22:33:19 -0500

    commit

--- provision-scripts/solr/provision.sh
+++ provision-scripts/solr/provision.sh
@@ -4,8 +4,8 @@ export DESTINATION_HOST=ld4502
 scp *.sh root@$DESTINATION_HOST:/root/.
 ssh root@$DESTINATION_HOST chmod u+x /root/*.sh
 
-scp $INSTALL_REPOSITORY/Solr/solr.tar root@$DESTINATION_HOST:/opt/solr/.
-scp $INSTALL_REPOSITORY/Solr/homedepot-solr-.*.tar.gz root@$DESTINATION_HOST:/opt/solr/.
+scp $INSTALL_REPOSITORY/Solr/solr.tar root@$DESTINATION_HOST:/root/.
+scp $INSTALL_REPOSITORY/Solr/homedepot-solr-.*.tar.gz root@$DESTINATION_HOST:/root/.
 
 ssh root@$DESTINATION_HOST /root/solr_setup_base.sh
 ssh root@$DESTINATION_HOST /root/solr_setup_env.sh
