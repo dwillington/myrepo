369415d53007b6be35f49f7914dc704ea7531091
Ashraf <AMJAD_ASHRAF@homedepot.com>
2016-12-13 22:27:42 -0500

    commit

--- provision-scripts/mysql/provision.sh
+++ provision-scripts/mysql/provision.sh
@@ -3,9 +3,10 @@ export DESTINATION_HOST=ld4505
 
 scp mysql_setup_base.sh root@$DESTINATION_HOST:/root/.
 scp mysql_setup_env.sh root@$DESTINATION_HOST:/root/.
-scp $INSTALL_REPOSITORY/*.sql root@$DESTINATION_HOST:/root/.
-scp $INSTALL_REPOSITORY/*.rpm root@$DESTINATION_HOST:~/.
+scp *.sql root@$DESTINATION_HOST:/root/.
 ssh root@$DESTINATION_HOST chmod u+x /root/*.sh
+
+scp $INSTALL_REPOSITORY/*.rpm root@$DESTINATION_HOST:~/.
 
 ssh root@$DESTINATION_HOST /root/mysql_setup_base.sh
 ssh root@$DESTINATION_HOST /root/mysql_setup_env.sh
