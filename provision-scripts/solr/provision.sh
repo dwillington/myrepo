62a1ca3aae037d02365058ed2279aef7a7c746cf
Ashraf <AMJAD_ASHRAF@homedepot.com>
2016-12-13 22:10:41 -0500

    commit

--- provision-scripts/solr/provision.sh
+++ provision-scripts/solr/provision.sh
@@ -1,8 +1,7 @@
 set INSTALL_REPOSITORY=/bamboo/data/thdutil/serversetup
 export DESTINATION_HOST=ld4502
 
-scp solr_setup_base.sh root@$DESTINATION_HOST:/root/.
-scp solr_setup_env.sh root@$DESTINATION_HOST:/root/.
+scp *.sh root@$DESTINATION_HOST:/root/.
 ssh root@$DESTINATION_HOST chmod u+x /root/*.sh
 
 ssh root@$DESTINATION_HOST /root/solr_setup_base.sh
