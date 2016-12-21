ca0424572e3e902ebb34ab0a02bc5fa80c856ff9
Ashraf <AMJAD_ASHRAF@homedepot.com>
2016-12-21 17:45:35 -0500

    commit

--- provision-scripts/apache/provision.sh
+++ provision-scripts/apache/provision.sh
@@ -2,10 +2,9 @@ export INSTALL_REPOSITORY=/bamboo/data/thdutil/serversetup/Apache
 export DESTINATION_HOST=$1
 
 scp apache_setup_base.sh root@$DESTINATION_HOST:/root/.
-scp apache_setup_env.sh root@$DESTINATION_HOST:/root/.
+scp apache_setup_on_docker.sh root@$DESTINATION_HOST:/root/.
 ssh root@$DESTINATION_HOST chmod u+x /root/*.sh
 
 scp $INSTALL_REPOSITORY/*.gz root@$DESTINATION_HOST:~/.
 
 ssh root@$DESTINATION_HOST /root/apache_setup_base.sh
-ssh root@$DESTINATION_HOST /root/apache_setup_env.sh
