0c45a6614b791b0d216fb9bd0a2bb65cbff80297
Ashraf <AMJAD_ASHRAF@homedepot.com>
2016-12-21 13:46:30 -0500

    commit

--- provision-scripts/aem/aem_setup_env.sh
+++ provision-scripts/aem/aem_setup_env.sh
@@ -4,7 +4,15 @@ echo "                "
 
 cd /opt/adobe/publish
 
-tar -xvf jdk1.7.0_75.tar
+echo " "
+echo "Start - Extract Jar file"
+echo " "
+
+tar -cvf aem.jar.gz
+
+echo " "
+echo "End - Extractjar file"
+echo " "
 
 echo "               "
 echo "Start - Setting Java Base"
@@ -17,13 +25,3 @@ update-alternatives --install "/usr/bin/javaws" "javaws" "/opt/adobe/publish/jdk
 echo "               "
 echo "End - Setting Java Base"
 echo "               "
-
-echo " "
-echo "Start - Extract Jar file"
-echo " "
-
-java -jar cq6-publish-p4503.jar -unpack
-
-echo " "
-echo "End - Extractjar file"
-echo " "
