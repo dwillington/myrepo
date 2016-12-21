8d9db1d1da655985d69b5fa58e88eac907bdf39f
Ashraf <AMJAD_ASHRAF@homedepot.com>
2016-12-21 15:45:30 -0500

    commit

--- provision-scripts/aem/provision.sh
+++ provision-scripts/aem/provision.sh
@@ -1,13 +1,13 @@
 export INSTALL_REPOSITORY=/bamboo/data/thdutil/serversetup/Aem
-export DESTINATION_HOST=ld4928
+export DESTINATION_HOST=$1
 
 scp aem_setup_base.sh root@$DESTINATION_HOST:/root/.
 scp aem_setup_env.sh root@$DESTINATION_HOST:/root/.
 scp aem_setup_publish.sh root@$DESTINATION_HOST:/root/.
 ssh root@$DESTINATION_HOST chmod u+x /root/*.sh
 
-#scp $INSTALL_REPOSITORY/aem.tar.gz root@$DESTINATION_HOST:~/.
+scp $INSTALL_REPOSITORY/aem.tar.gz root@$DESTINATION_HOST:~/.
 
-#ssh root@$DESTINATION_HOST /root/aem_setup_base.sh
-#ssh root@$DESTINATION_HOST /root/aem_setup_env.sh
-#ssh root@$DESTINATION_HOST /root/aem_setup_publish.sh
+ssh root@$DESTINATION_HOST /root/aem_setup_base.sh
+ssh root@$DESTINATION_HOST /root/aem_setup_env.sh
+ssh root@$DESTINATION_HOST /root/aem_setup_publish.sh
