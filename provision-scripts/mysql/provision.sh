bcfab53bc710a62e4858dc8cec9795a9a0d0eab3
Ashraf <AMJAD_ASHRAF@homedepot.com>
2016-12-25 18:49:53 -0500

    commit

--- provision-scripts/mysql/provision.sh
+++ provision-scripts/mysql/provision.sh
@@ -1,14 +1,15 @@
+export SSH_ARGS=-o StrictHostKeyChecking=no
 export INSTALL_REPOSITORY=/bamboo/data/thdutil/serversetup/MySQL
 export DESTINATION_HOST=$1
 
 #sshpass -p "password" ssh-copy-id root@$1
 
-scp mysql_setup_base.sh root@$DESTINATION_HOST:/root/.
-scp mysql_setup_env.sh root@$DESTINATION_HOST:/root/.
-scp *.sql root@$DESTINATION_HOST:/root/.
-ssh root@$DESTINATION_HOST chmod u+x /root/*.sh
+scp $SSH_ARGS mysql_setup_base.sh root@$DESTINATION_HOST:/root/.
+scp $SSH_ARGS mysql_setup_env.sh root@$DESTINATION_HOST:/root/.
+scp $SSH_ARGS *.sql root@$DESTINATION_HOST:/root/.
+ssh $SSH_ARGS root@$DESTINATION_HOST chmod u+x /root/*.sh
 
-scp $INSTALL_REPOSITORY/*.rpm root@$DESTINATION_HOST:~/.
+scp $SSH_ARGS $INSTALL_REPOSITORY/*.rpm root@$DESTINATION_HOST:~/.
 
-ssh root@$DESTINATION_HOST /root/mysql_setup_base.sh
-ssh root@$DESTINATION_HOST /root/mysql_setup_env.sh
+ssh $SSH_ARGS root@$DESTINATION_HOST /root/mysql_setup_base.sh
+ssh $SSH_ARGS root@$DESTINATION_HOST /root/mysql_setup_env.sh
