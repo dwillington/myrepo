bef77b7bef962c33ea63716b3358ba958b65033b
Ashraf <AMJAD_ASHRAF@homedepot.com>
2016-12-25 18:41:49 -0500

    commit

--- provision-scripts/mysql/mysql_setup_env.sh
+++ provision-scripts/mysql/mysql_setup_env.sh
@@ -8,7 +8,11 @@ yum -y remove MariaDB-common
 cd /opt/mysql
 
 rpm -ivh ~/MySQL-shared-5.5.30-1.el6.x86_64.rpm
+
+yum install libaio -y
 rpm -ivh ~/MySQL-client-5.5.30-1.el6.x86_64.rpm
+
+yum install net-tools -y
 rpm -ivh ~/MySQL-server-5.5.30-1.el6.x86_64.rpm
 
 service mysql start
