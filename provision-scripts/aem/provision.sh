6f210c6aac4cf94fea8b27d96bcc9372b6ba2aef
Ashraf <AMJAD_ASHRAF@homedepot.com>
2016-12-25 19:24:09 -0500

    commit

--- provision-scripts/aem/provision.sh
+++ provision-scripts/aem/provision.sh
@@ -1,14 +1,15 @@
+export SSH_ARGS='-o StrictHostKeyChecking=no'
 export INSTALL_REPOSITORY=/bamboo/data/thdutil/serversetup/Aem
 export DESTINATION_HOST=$1
 #sshpass -p "password" ssh-copy-id root@$1
 
-scp aem_setup_base.sh root@$DESTINATION_HOST:/root/.
-scp aem_setup_env.sh root@$DESTINATION_HOST:/root/.
-scp aem_setup_publish.sh root@$DESTINATION_HOST:/root/.
-ssh root@$DESTINATION_HOST chmod u+x /root/*.sh
+scp $SSH_ARGS aem_setup_base.sh root@$DESTINATION_HOST:/root/.
+scp $SSH_ARGS aem_setup_env.sh root@$DESTINATION_HOST:/root/.
+scp $SSH_ARGS aem_setup_publish.sh root@$DESTINATION_HOST:/root/.
+ssh $SSH_ARGS root@$DESTINATION_HOST chmod u+x /root/*.sh
 
-#scp $INSTALL_REPOSITORY/aem.tar.gz root@$DESTINATION_HOST:~/.
+scp $SSH_ARGS $INSTALL_REPOSITORY/aem.tar.gz root@$DESTINATION_HOST:~/.
 
-ssh root@$DESTINATION_HOST /root/aem_setup_base.sh
-ssh root@$DESTINATION_HOST /root/aem_setup_env.sh
-ssh root@$DESTINATION_HOST /root/aem_setup_publish.sh
+ssh $SSH_ARGS root@$DESTINATION_HOST /root/aem_setup_base.sh
+ssh $SSH_ARGS root@$DESTINATION_HOST /root/aem_setup_env.sh
+ssh $SSH_ARGS root@$DESTINATION_HOST /root/aem_setup_publish.sh
