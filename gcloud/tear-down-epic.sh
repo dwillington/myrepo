643e209f666569d0de944c523e816edb7718d20d
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-01-06 14:39:43 -0500

    commit

--- gcloud/tear-down-epic.sh
+++ gcloud/tear-down-epic.sh
@@ -4,7 +4,7 @@ if [ -z ${epic_name+x} ]; then
     echo "epic_name is unset";
 else
     echo "epic-name is set to '$epic_name'";
-    delete_vm() $epic_name solr
+    delete_vm $epic_name solr
 fi
 
 
