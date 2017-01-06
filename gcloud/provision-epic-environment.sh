a3a9aadc3fb6cddd6ae2a184e13aa14a75a0951b
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-01-06 15:00:28 -0500

    commit

--- gcloud/provision-epic-environment.sh
+++ gcloud/provision-epic-environment.sh
@@ -1,12 +1,13 @@
-#! /bin/bash
+#!/bin/bash
 
 create_vm()
 {
     gcloud compute instances create $1-$2 \
-    --tags $2-ca ^
-    --image-family centos-7 ^
-    --image-project centos-cloud ^
-    --boot-disk-size 50 ^
+    --zone=us-west1-b \
+    --tags $2-ca \
+    --image-family centos-7 \
+    --image-project centos-cloud \
+    --boot-disk-size 50 \
     --metadata-from-file startup-script=./startup.sh
 }
 
@@ -14,14 +15,9 @@ if [ -z ${epic_name+x} ]; then
     echo "epic_name is unset";
 else
     echo "epic-name is set to '$epic_name'";
-    create_vm() $epic_name aem
-    create_vm() $epic_name apache
-    create_vm() $epic_name hybris
-    create_vm() $epic_name mysql
-    create_vm() $epic_name solr
+    create_vm $epic_name aem
+    create_vm $epic_name apache
+    create_vm $epic_name hybris
+    create_vm $epic_name mysql
+    create_vm $epic_name solr
 fi
-
-
-
-
-
