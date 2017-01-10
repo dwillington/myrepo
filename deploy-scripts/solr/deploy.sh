13c161f64c9976c812a87dfee9b455bf1290adaf
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-01-10 17:40:24 -0500

    commit

--- deploy-scripts/solr/deploy.sh
+++ deploy-scripts/solr/deploy.sh
@@ -8,7 +8,7 @@ fi
 export ARTIFACT_LOCATION=/mnt/gcs-bucket/ci-builds/epic-builds/$1/solr
 export DESTINATION_HOST=$1-solr
 
-scp $SSH_ARGS ARTIFACT_LOCATION/homedepot-solr-0.0.1-SNAPSHOT.tar.gz root@$DESTINATION_HOST:/root/.
-scp $SSH_ARGS deploy-local.sh root@$DESTINATION_HOST-solr:/root/.
+scp $SSH_ARGS $ARTIFACT_LOCATION/homedepot-solr-0.0.1-SNAPSHOT.tar.gz root@$DESTINATION_HOST:/root/.
+scp $SSH_ARGS deploy-local.sh root@$DESTINATION_HOST:/root/.
 
 ssh $SSH_ARGS root@$DESTINATION_HOST /root/deploy-local.sh
