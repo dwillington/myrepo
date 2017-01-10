e38bbd7bc21eced6565eb1bfe626b9829f4bc0b8
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-01-10 17:30:32 -0500

    commit

---
+++ deploy-scripts/solr/deploy.sh
@@ -0,0 +1,14 @@
+export SSH_ARGS='-o StrictHostKeyChecking=no'
+
+if [ $# -lt 1 ]; then
+  echo 1>&2 "$0: not enough arguments"
+  exit 2
+fi
+
+export ARTIFACT_LOCATION=/mnt/gcs-bucket/ci-builds/epic-builds/$1/solr
+export DESTINATION_HOST=$1-solr
+
+scp $SSH_ARGS ARTIFACT_LOCATION/homedepot-solr-0.0.1-SNAPSHOT.tar.gz root@$DESTINATION_HOST:/root/.
+scp $SSH_ARGS deploy-local.sh root@$DESTINATION_HOST-solr:/root/.
+
+ssh $SSH_ARGS root@$DESTINATION_HOST /root/deploy-local.sh
