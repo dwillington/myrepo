4e29a9ac63815a87eab920ff31c7dccabd6de631
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-01-12 23:18:49 -0500

    commit

--- job-dsl-scripts/aseedjob.groovy
+++ job-dsl-scripts/aseedjob.groovy
@@ -1,71 +1,71 @@
-job("create-epic-environment") {
-    parameters {
-        stringParam('epic_name')
-    }
-    scm {
-        git {
-            remote {
-                url("https://github.com/dwillington/myrepo.git")
-                credentials('1a678709-ecf9-42a6-b1ea-82acbcab44bb')
-                branch('gcloud')
-            }
-        }
-    }
-    steps {
-        shell('gcloud-scripts/create-epic-environment.sh $epic_name')
-    }
-    publishers {
-        logRotator {
-            numToKeep(10)
-        }
-    }
-}
+// job("create-epic-environment") {
+    // parameters {
+        // stringParam('epic_name')
+    // }
+    // scm {
+        // git {
+            // remote {
+                // url("https://github.com/dwillington/myrepo.git")
+                // credentials('1a678709-ecf9-42a6-b1ea-82acbcab44bb')
+                // branch('gcloud')
+            // }
+        // }
+    // }
+    // steps {
+        // shell('gcloud-scripts/create-epic-environment.sh $epic_name')
+    // }
+    // publishers {
+        // logRotator {
+            // numToKeep(10)
+        // }
+    // }
+// }
 
-job("provision-epic-environment") {
-    parameters {
-        stringParam('epic_name')
-    }
-    scm {
-        git {
-            remote {
-                url("https://github.com/dwillington/myrepo.git")
-                credentials('1a678709-ecf9-42a6-b1ea-82acbcab44bb')
-                branch('gcloud')
-            }
-        }
-    }
-    steps {
-        shell('gcloud-scripts/provision-epic-environment.sh $epic_name')
-    }
-    publishers {
-        logRotator {
-            numToKeep(10)
-        }
-    }
-}
+// job("provision-epic-environment") {
+    // parameters {
+        // stringParam('epic_name')
+    // }
+    // scm {
+        // git {
+            // remote {
+                // url("https://github.com/dwillington/myrepo.git")
+                // credentials('1a678709-ecf9-42a6-b1ea-82acbcab44bb')
+                // branch('gcloud')
+            // }
+        // }
+    // }
+    // steps {
+        // shell('gcloud-scripts/provision-epic-environment.sh $epic_name')
+    // }
+    // publishers {
+        // logRotator {
+            // numToKeep(10)
+        // }
+    // }
+// }
 
-job("tear-down-epic-environment") {
-    parameters {
-        stringParam('epic_name')
-    }
-    scm {
-        git {
-            remote {
-                url("https://github.com/dwillington/myrepo.git")
-                credentials('1a678709-ecf9-42a6-b1ea-82acbcab44bb')
-                branch('gcloud')
-            }
-        }
-    }
-    steps {
-        shell('gcloud-scripts/tear-down-epic-environment.sh $epic_name')
-    }
-    publishers {
-        logRotator {
-            numToKeep(10)
-        }
-    }
-}
+// job("tear-down-epic-environment") {
+    // parameters {
+        // stringParam('epic_name')
+    // }
+    // scm {
+        // git {
+            // remote {
+                // url("https://github.com/dwillington/myrepo.git")
+                // credentials('1a678709-ecf9-42a6-b1ea-82acbcab44bb')
+                // branch('gcloud')
+            // }
+        // }
+    // }
+    // steps {
+        // shell('gcloud-scripts/tear-down-epic-environment.sh $epic_name')
+    // }
+    // publishers {
+        // logRotator {
+            // numToKeep(10)
+        // }
+    // }
+// }
 
 createPollAndDeployJob('epic1', 'solr')
 
