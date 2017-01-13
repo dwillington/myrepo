f33fb971bb7d5211d19f06fb4a0fe0ee40ab44f1
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-01-12 21:43:12 -0500

    commit

--- job-dsl-scripts/aseedjob.groovy
+++ job-dsl-scripts/aseedjob.groovy
@@ -69,3 +69,32 @@ job("tear-down-epic-environment") {
         }
     }
 }
+
+job("poll-and-deploy-epic1-solr") {
+    scm {
+        git {
+            remote {
+                url("https://github.com/dwillington/myrepo.git")
+                credentials('1a678709-ecf9-42a6-b1ea-82acbcab44bb')
+                branch('gcloud')
+            }
+        }
+    }
+    configure { project ->
+        project / triggers / "org.jenkinsci.plugins.fstrigger.triggers.FolderContentTrigger" (plugin: "fstrigger@0.39") {
+            spec("* * * * *")
+            path('/mnt/gcs-bucket/ci-builds/epic-builds/epic1/solr')
+            excludeCheckLastModificationDate('false')
+            excludeCheckContent('true')
+            excludeCheckFewerOrMoreFiles('false')
+        }
+    }
+    steps {
+        shell('deploy-scripts/solr/deploy.sh epic1-solr')
+    }
+    publishers {
+        logRotator {
+            numToKeep(10)
+        }
+    }
+}
