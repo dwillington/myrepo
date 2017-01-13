8565fb25848bddfb08362a2b87e583f6c03e8616
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-01-12 21:07:21 -0500

    commit

--- provision-scripts/hybris/provision.sh
+++ provision-scripts/hybris/provision.sh
@@ -3,6 +3,11 @@ export INSTALL_REPOSITORY=/bamboo/data/thdutil/serversetup/Hybris
 export DESTINATION_HOST=$1
 #sshpass -p "password" ssh-copy-id root@$1
 
+/bin/cp -rf local.properties.base.orig local.properties.base
+export epic_name=${DESTINATION_HOST%-*}
+sed -i -e "s/mysql-hostname/$epic_name-mysql/g" local.properties.base
+echo "setting mysql hostname to $epic_name-mysql in local.properties.base"
+
 scp $SSH_ARGS *.sh *.base *.pfx *.jck root@$DESTINATION_HOST:~/.
 
 ssh $SSH_ARGS root@$DESTINATION_HOST chmod u+x /root/*.sh
