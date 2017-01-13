ee7cb7701857d19a7724786dcc488b310943229c
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-01-12 20:29:29 -0500

    commit

--- gcloud-scripts/create-epic-environment.sh
+++ gcloud-scripts/create-epic-environment.sh
@@ -19,8 +19,8 @@ fi
 export epic_name=$1
 echo "epic_name is set to '$epic_name'";
 
-#create_vm $epic_name mysql
-#create_vm $epic_name hybris
-#create_vm $epic_name solr
-#create_vm $epic_name aem
-#create_vm $epic_name apache
+create_vm $epic_name mysql
+create_vm $epic_name hybris
+create_vm $epic_name solr
+create_vm $epic_name aem
+create_vm $epic_name apache
