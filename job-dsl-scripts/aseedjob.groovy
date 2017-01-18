51cd8719e82f172827cd2880b7823cae7449c8ff
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-01-17 19:12:21 -0500

    commit

--- job-dsl-scripts/aseedjob.groovy
+++ job-dsl-scripts/aseedjob.groovy
@@ -67,6 +67,27 @@ job("tear-down-epic-environment") {
     }
 }
 
+job("deploy-${epic_name}-${project_name}") {
+    scm {
+        git {
+            remote {
+                url("https://github.com/dwillington/myrepo.git")
+                credentials('1a678709-ecf9-42a6-b1ea-82acbcab44bb')
+                branch('gcloud')
+            }
+        }
+    }
+    steps {
+        shell("echo deploy-scripts/${project_name}/deploy.sh ${epic_name}")
+        // shell("deploy-scripts/${project_name}/deploy.sh ${epic_name}")
+    }
+    publishers {
+        logRotator {
+            numToKeep(10)
+        }
+    }
+}
+
 createPollAndDeployJob('epic1', 'solr')
 
 def createPollAndDeployJob(epic_name, project_name) {
