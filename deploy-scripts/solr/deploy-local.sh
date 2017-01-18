7e84b9b558b41ec13a118b7f905f0811d4e55293
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-01-17 22:14:55 -0500

    commit

--- deploy-scripts/solr/deploy-local.sh
+++ deploy-scripts/solr/deploy-local.sh
@@ -4,6 +4,9 @@ cd /opt/solr/build
 tar -zxvf /root/homedepot-solr-0.0.1-SNAPSHOT.tar.gz
 rm -rf /opt/solr/solr-home
 mv /opt/solr/build/solr-home /opt/solr
+echo "--------------------------------------------------"
+echo "sleeping 2m while waiting for solr to stop..."
+echo "--------------------------------------------------"
 sleep 2m
 /root/start_solr.sh
 find /opt/solr/solr-home_backup/ -name "*.tar.gz" -mtime +7 -print -exec rm {} \;
