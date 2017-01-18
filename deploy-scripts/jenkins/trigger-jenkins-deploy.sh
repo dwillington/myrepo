2255e1e3cb928981850e0f99526927861ee83b22
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-01-18 17:55:39 -0500

    commit

--- deploy-scripts/jenkins/trigger-jenkins-deploy.sh
+++ deploy-scripts/jenkins/trigger-jenkins-deploy.sh
@@ -6,6 +6,9 @@ fi
 export epic_name=$1
 export project_name=$2
 
+export http_proxy=http://str-www-proxy2-qa.homedepot.com:8080
+export https_proxy=http://str-www-proxy2-qa.homedepot.com:8080
+
 export JENKINS_SERVER=104.198.103.152
 export CRUMB=$(curl --user admin:76a4a60136ff3f563f7ad5c3fd52552d http://$JENKINS_SERVER/crumbIssuer/api/xml?xpath=concat\(//crumbRequestField,%22:%22,//crumb\))
 echo curl -X POST http://$JENKINS_SERVER/job/deploy-epic-project/build \
