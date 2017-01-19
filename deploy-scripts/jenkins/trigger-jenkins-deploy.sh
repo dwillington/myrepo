b2b1f79c301147e457d9ac8e0889837d67f7b7eb
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-01-19 16:39:34 -0500

    commit

--- deploy-scripts/jenkins/trigger-jenkins-deploy.sh
+++ deploy-scripts/jenkins/trigger-jenkins-deploy.sh
@@ -8,12 +8,15 @@ fi
 
 export epic_name=epic2
 export project_name=solr
-export json=\''{"parameter": [{"name":"epic_name", "value":"'$epic_name'"}, {"name":"project_name", "value":"'$project_name'"}]}'\'
-echo $json
 
 export JENKINS_SERVER=104.198.103.152
 export CRUMB=$(curl --user admin:76a4a60136ff3f563f7ad5c3fd52552d http://$JENKINS_SERVER/crumbIssuer/api/xml?xpath=concat\(//crumbRequestField,%22:%22,//crumb\))
-curl -X POST http://$JENKINS_SERVER/job/deploy-epic-project/build \
-     -H "$CRUMB" \
+curl -H "$CRUMB" \
      --user admin:76a4a60136ff3f563f7ad5c3fd52552d \
-     --data-urlencode json=$json
+     -X POST http://$JENKINS_SERVER/job/deploy-epic-project/buildWithParameters?epic_name=$epic_name\&project_name=$project_name
+
+# the following will not work because $epic_name/$project_name are in '' so they will be taken literally and not substitued
+#curl -X POST http://$JENKINS_SERVER/job/deploy-epic-project/build \
+#     -H "$CRUMB" \
+#     --user admin:76a4a60136ff3f563f7ad5c3fd52552d \
+#     --data-urlencode json='{"parameter": [{"name":"epic_name", "value":"$epic_name"}, {"name":"project_name", "value":"$project_name"}]}'
