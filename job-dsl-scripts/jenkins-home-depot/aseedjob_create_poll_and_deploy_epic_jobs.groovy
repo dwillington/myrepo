288ca439fac37b2ad9fe496f20f374aee66c9010
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-01-20 17:44:17 -0500

    commit

--- job-dsl-scripts/jenkins-home-depot/aseedjob_create_poll_and_deploy_epic_jobs.groovy
+++ job-dsl-scripts/jenkins-home-depot/aseedjob_create_poll_and_deploy_epic_jobs.groovy
@@ -35,6 +35,40 @@ if(binding.variables.containsKey("epic_name")) {
         }
     }
 
+    job("poll-and-deploy-${epic_name}-apache") {
+        scm {
+            git {
+                remote {
+                    url("http://stash.homedepot.ca/scm/hdca/apache.git")
+                    credentials('axa8962-credentials')
+                    branch("$epic_name")
+                }
+            }
+        }
+        // triggers {
+            // scm('H/10 * * * *')
+        // }
+        steps {
+            maven {
+                rootPOM('pom.xml')
+                goals("clean assembly:assembly -Pbuild-httpd-config,qp -DproxySet=true -DproxyHost=str-www-proxy2-qa -DproxyPort=8080")
+                mavenInstallation('apache-maven-3.3.9')
+            }
+            shell(
+                "export HTTP_PROXY=http://str-www-proxy2-qa.homedepot.com:8080\n" + 
+                "export HTTPS_PROXY=http://str-www-proxy2-qa.homedepot.com:8080\n" + 
+                "/root/google-cloud-sdk/bin/gsutil cp target/homedepot-httpd-0.0.1-SNAPSHOT.tar.gz gs://np-cadotcom.appspot.com/ci-builds/epic-builds/epic2/apache/homedepot-httpd-0.0.1-SNAPSHOT.tar.gz\n" + 
+                "/root/myrepo/deploy-scripts/jenkins/trigger-jenkins-deploy.sh epic2 apache\n"
+                 )
+        }
+        publishers {
+            logRotator {
+                numToKeep(10)
+            }
+        }
+    }
+
+
 }
 
 
