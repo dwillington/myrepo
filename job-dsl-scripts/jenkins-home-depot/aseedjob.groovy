eb27b3ffb6b4b053b479427300c901c2c3577deb
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-01-20 15:18:48 -0500

    commit

---
+++ job-dsl-scripts/jenkins-home-depot/aseedjob.groovy
@@ -0,0 +1,28 @@
+// a-seed-job
+// https://github.com/dwillington/myrepo.git
+// job-dsl-scripts/jenkins-home-depot/**/*.groovy
+
+job("create-poll-and-build-epic-jobs") {
+    parameters {
+        stringParam('epic_name')
+        stringParam('project_name')
+    }
+    scm {
+        git {
+            remote {
+                url("https://github.com/dwillington/myrepo.git")
+                credentials('dwillington-credentials')
+                branch('gcloud')
+            }
+        }
+    }
+    dsl {
+        external('job-dsl-scripts/jenkins-home-depot/aseedjob_create_poll_and_deploy_epic_jobs.groovy')
+    }
+    publishers {
+        logRotator {
+            numToKeep(10)
+        }
+    }
+}
+
