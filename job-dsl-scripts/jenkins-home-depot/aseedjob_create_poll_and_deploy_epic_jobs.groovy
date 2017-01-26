17fa2b8b7b6948fb047e4f69c03fcd5605384f55
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-01-26 13:33:47 -0500

    commit

--- job-dsl-scripts/jenkins-home-depot/aseedjob_create_poll_and_deploy_epic_jobs.groovy
+++ job-dsl-scripts/jenkins-home-depot/aseedjob_create_poll_and_deploy_epic_jobs.groovy
@@ -98,8 +98,9 @@ if(binding.variables.containsKey("epic_name")) {
             shell(
                 "export HTTP_PROXY=http://str-www-proxy2-qa.homedepot.com:8080\n" + 
                 "export HTTPS_PROXY=http://str-www-proxy2-qa.homedepot.com:8080\n" + 
-                "/root/google-cloud-sdk/bin/gsutil cp homedepot-apps-0.0.1-SNAPSHOT.zip gs://np-cadotcom.appspot.com/ci-builds/epic-builds/epic2/aem/homedepot-apps-0.0.1-SNAPSHOT.zip\n" + 
-                "/root/myrepo/deploy-scripts/jenkins/trigger-jenkins-deploy.sh epic2 aem\n"
+                // homedepot-apps/target/homedepot.ca.homedepot-apps.zip
+                // "/root/google-cloud-sdk/bin/gsutil cp homedepot-apps-0.0.1-SNAPSHOT.zip gs://np-cadotcom.appspot.com/ci-builds/epic-builds/epic2/aem/homedepot-apps-0.0.1-SNAPSHOT.zip\n" + 
+                // "/root/myrepo/deploy-scripts/jenkins/trigger-jenkins-deploy.sh epic2 aem\n"
             )
         }
         publishers {
