c6ea6d23991f3484ebaa50ebadfa314864fe53fa
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-01-25 21:26:11 -0500

    commit

---
+++ deploy-scripts/aem/deploy-local.sh
@@ -0,0 +1,10 @@
+#/opt/adobe/publish/crx-quickstart/bin/stop
+
+#cd /opt/adobe/publish
+#. ./setenv.sh 
+
+#/opt/adobe/publish/crx-quickstart/bin/start
+
+curl -u admin:admin -F file=@"homedepot-apps-0.0.1-SNAPSHOT.zip" -F name="homedepot-apps" -F force=true -F install=true http://localhost:4503/crx/packmgr/service.jsp 
+
+tail -100 /opt/adobe/publish/crx-quickstart/logs/stdout.log
