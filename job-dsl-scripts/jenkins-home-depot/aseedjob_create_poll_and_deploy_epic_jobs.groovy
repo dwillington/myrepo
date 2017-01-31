d2cb34e49f57edd813b24a18f6fd9b6dd96800c3
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-01-30 21:19:56 -0500

    commit

--- job-dsl-scripts/jenkins-home-depot/aseedjob_create_poll_and_deploy_epic_jobs.groovy
+++ job-dsl-scripts/jenkins-home-depot/aseedjob_create_poll_and_deploy_epic_jobs.groovy
@@ -99,7 +99,7 @@ if(binding.variables.containsKey("epic_name")) {
                 rootPOM('pom.xml')
                 // goals("clean install -Pqp -Dcrx.url=http://ln0bd7.homedepot.com:4502 -DproxySet=true -DproxyHost=str-www-proxy2-qa -DproxyPort=8080")
                 // TODO use an environment variable for the ip address
-                goals("install -Pqp -Dcrx.failOnError=false -Dcrx.url=http://172.24.102.175:4503 -DproxySet=true -DproxyHost=str-www-proxy2-qa -DproxyPort=8080 -Dcrx.password='admin' -pl '!homedepot-integration-tests,!test-content'")
+                goals("install -Pqp -Dcrx.failOnError=false -Dcrx.url=http://172.24.102.175:4503 -DproxySet=true -DproxyHost=str-www-proxy2-qa -DproxyPort=8080 -Dcrx.password='admin' -pl '!homedepot-integration-tests'")
                 // -pl '!test-content,!integration-tests,!integration-test-runners'
                 mavenInstallation('apache-maven-3.3.9')
                 localRepository(LocalRepositoryLocation.LOCAL_TO_WORKSPACE)
