a87d8a9a739176b55ab1b231c098d1bf77dabaf5
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-02-22 18:43:59 -0500

    commit

--- provision-scripts/mysql/mysql_setup_env_5.7.sh
+++ provision-scripts/mysql/mysql_setup_env_5.7.sh
@@ -1,20 +1,3 @@
-ip6tables -A INPUT -i eth0 -p tcp --dport 80 -j ACCEPT
-ip6tables -A INPUT -i eth0 -p tcp --dport 3306 -j ACCEPT
-ip6tables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3306
-
-iptables -D INPUT -i eth0 -p tcp --dport 80 -j ACCEPT
-iptables -D INPUT -i eth0 -p tcp --dport 3306 -j ACCEPT
-iptables -D PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3306
-
-iptables -A INPUT -i eth0 -p tcp --dport 80 -j ACCEPT
-iptables -A INPUT -i eth0 -p tcp --dport 3306 -j ACCEPT
-iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3306
-
-iptables -t nat -L
-
-iptables -t nat -A PREROUTING -p tcp -d 0.0.0.0 --dport 80 -j DNAT --to-destination 127.0.0.1:3306
-
-
 echo " "
 echo "Starting MySQL Setup"
 echo " "
