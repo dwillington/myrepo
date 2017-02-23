2277435701cd2b4801e51f623e42c81480d383a7
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-02-22 21:38:18 -0500

    commit

--- provision-scripts/hybris/provision.sh
+++ provision-scripts/hybris/provision.sh
@@ -7,7 +7,7 @@ export SSH_ARGS='-o StrictHostKeyChecking=no'
 export INSTALL_REPOSITORY=/bamboo/data/thdutil/serversetup/Hybris-6.2
 export DESTINATION_HOST=$1
 
-scp $SSH_ARGS *.sh *.orig *.txt root@$DESTINATION_HOST:~/.
+scp $SSH_ARGS *.sh *.orig *.base *.txt root@$DESTINATION_HOST:~/.
 
 ssh $SSH_ARGS root@$DESTINATION_HOST chmod u+x /root/*.sh
 
