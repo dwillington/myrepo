49c6d8b380324425bf589a6a4303bc91b03116a7
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-02-28 19:54:12 -0500

    commit

--- provision-scripts/hybris/mysql_post_setup.sh
+++ provision-scripts/hybris/mysql_post_setup.sh
@@ -1,5 +1,6 @@
 export DESTINATION_HOST=`hostname`
 export epic_name=${DESTINATION_HOST%-*}
 
-echo "uppdate solrendpointurl set p_url=\"http://$epic_name-solr/solr\" where p_solrserverconfig=(SELECT pk FROM solrserverconfig where p_name='homedepotcaSolrServerConfig');" > /root/solrendpointurl.sql
+echo "use hybrisdbuser;" > /root/solrendpointurl.sql
+echo "uppdate solrendpointurl set p_url=\"http://$epic_name-solr/solr\" where p_solrserverconfig=(SELECT pk FROM solrserverconfig where p_name='homedepotcaSolrServerConfig');" >> /root/solrendpointurl.sql
 mysql -uroot -pjasp3r91 < /root/solrendpointurl.sql
