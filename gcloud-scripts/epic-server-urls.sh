1170121caf8814df7ce432b9ddb1d114a75f15e3
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-03-01 22:05:22 -0500

    commit

--- gcloud-scripts/epic-server-urls.sh
+++ gcloud-scripts/epic-server-urls.sh
@@ -6,24 +6,34 @@ if [ $# -lt 1 ]; then
 fi
 
 export epic_name=$1
-echo "epic_name is set to '$epic_name'";
+#echo "epic_name is set to '$epic_name'";
 
 gcloud compute instances list --regexp=$epic_name.* > out.txt
 sed -ie '1d' out.txt # remove line 1
 awk '{print $9}' out.txt > out1.txt # only keep column 9, which is the ip address
 mv -f out1.txt out.txt
 sed -i -e 's/^/http:\/\//' out.txt
-sed -i '1s/$/\/login/' out.txt
+sed -i '1s/$/\/libs\/granite\/core\/content\/login.html/' out.txt
+sed -i '2s/$/\/en\/home.html/' out.txt
 sed -i '3s/$/\/hmc\/hybris/' out.txt
-sed -i '4s/$/\/phpmyadmin/' out.txt
-sed -i '5s/$/\/solr/' out.txt
+sed -i '4s/$/\/phpmyadmin\//' out.txt
+sed -i '5s/$/\/solr\//' out.txt
 
 if [ $# -lt 2 ]
 then
   cat out.txt
 else
   export project_name=$2
-  case project_name in
+  case $project_name in
+    "aem" )
+        sed -n 1p out.txt;;
+    "apache" )
+        sed -n 2p out.txt;;
+    "hybris" )
+        sed -n 3p out.txt;;
     "mysql" )
         sed -n 4p out.txt;;
+    "solr" )
+        sed -n 5p out.txt;;
+  esac
 fi
