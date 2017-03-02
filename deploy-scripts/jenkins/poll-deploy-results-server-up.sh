df82234a1e2de386a5fdf115d0a53f883de5e263
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-03-02 10:15:00 -0500

    commit

--- deploy-scripts/jenkins/poll-deploy-results-server-up.sh
+++ deploy-scripts/jenkins/poll-deploy-results-server-up.sh
@@ -10,12 +10,36 @@ fi
 export epic_name=$1
 export project_name=$2
 
+export SLEEP_PERIOD=2
 export server_url=`gcloud-scripts/epic-server-urls.sh $epic_name $project_name`
-export http_code=`curl -s -o /dev/null -I -w "%{http_code}" $server_url`
+
+# export http_code=000
+
+# we may wish to change this to ! -eq 200
+# while [ "$x" -lt 30 -a ! "$http_code" -eq "000" ]; do
+    # export http_code=`curl --max-time 5 -s -o /dev/null -I -w "%{http_code}" $server_url`
+    # x=$((x+1))
+    # sleep $SLEEP_PERIOD
+# done
+
+while true
+do
+  http_code=`curl --max-time 5 -s -o /dev/null -I -w "%{http_code}" $server_url`
+  x=$((x+1))
+  if [ $http_code -eq 200 ]; then
+    echo "Got 200! All done!"
+    break
+  elif [ "$x" -gt 30 ]; then
+    echo "Giving up!"
+    break
+  else
+    echo "Got $STATUS :( Not done yet..."
+  fi
+  sleep $SLEEP_PERIOD
+done
 
 if [ "$http_code" -eq "200" ]; then
    exit 0;
 else
-   echo "http_code: $http_code";
    exit 1;
 fi
