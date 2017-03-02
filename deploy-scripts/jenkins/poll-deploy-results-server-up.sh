c72e2ece7f042f3cda3e50c8fd38f81d3eaff489
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-03-01 22:37:22 -0500

    commit

---
+++ deploy-scripts/jenkins/poll-deploy-results-server-up.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+export http_proxy=http://str-www-proxy2-qa.homedepot.com:8080
+export https_proxy=http://str-www-proxy2-qa.homedepot.com:8080
+
+if [ $# -lt 2 ]; then
+  echo 1>&2 "$0: provide epic_name and project_name"
+  exit 2
+fi
+
+export epic_name=$1
+export project_name=$2
+
+export server_url=`gcloud-scripts/epic-server-urls.sh $epic_name $project_name`
+export http_code =`curl -s -o /dev/null -I -w "%{http_code}" $server_url`
+echo $http_code
