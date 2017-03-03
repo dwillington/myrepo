279ae23df26054853fe4fb974200db25938484f1
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-03-03 16:59:16 -0500

    commit

--- deploy-scripts/jenkins/poll-deploy-results-server-up.sh
+++ deploy-scripts/jenkins/poll-deploy-results-server-up.sh
@@ -3,14 +3,20 @@ export http_proxy=http://str-www-proxy2-qa.homedepot.com:8080
 export https_proxy=http://str-www-proxy2-qa.homedepot.com:8080
 
 if [ $# -lt 2 ]; then
-  echo 1>&2 "$0: provide epic_name and project_name"
+  echo 1>&2 "$0: provide epic_name project_name [num_attempts]"
   exit 2
 fi
 
 export epic_name=$1
 export project_name=$2
 
-export SLEEP_PERIOD=2
+export SLEEP_PERIOD=5
+export NUM_ATTEMPTS=5
+if [ $# -gt 2 ]
+then
+    export NUM_ATTEMPTS=$3
+else
+
 export server_url=`gcloud-scripts/epic-server-urls.sh $epic_name $project_name`
 
 #https://coderwall.com/p/taqiyg/use-http-status-codes-from-curl
@@ -21,7 +27,7 @@ do
   if [ $http_code -eq 200 ]; then
     echo "Got 200! All done!"
     break
-  elif [ "$x" -gt 30 ]; then
+  elif [ "$x" -gt $NUM_ATTEMPTS ]; then
     echo "Giving up!"
     break
   else
