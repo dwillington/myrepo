a09238cab79ba270bbf4fc58a289fc2ec1ae78d7
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-03-09 21:56:31 -0500

    commit

--- deploy-scripts/jenkins/poll-deploy-job-results.sh
+++ deploy-scripts/jenkins/poll-deploy-job-results.sh
@@ -32,7 +32,7 @@ sed -i 's/$/api\/json?pretty=true/' $random_folder/out.txt
 deploy_job_url=`cat $random_folder/out.txt`
 echo $deploy_job_url
 
-while [ "$x" -lt 30 ]; do
+while [ $x -lt 30 ]; do
     curl -H "$CRUMB" \
         --user admin:76a4a60136ff3f563f7ad5c3fd52552d \
         -X POST $deploy_job_url > $random_folder/deploy_job.txt
