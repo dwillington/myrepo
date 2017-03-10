e152c22c49b6180b5c66d759819d3280576b0326
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-03-09 22:33:25 -0500

    commit

--- deploy-scripts/jenkins/poll-deploy-job-results.sh
+++ deploy-scripts/jenkins/poll-deploy-job-results.sh
@@ -20,6 +20,8 @@ dos2unix $random_folder/queue_item_url.txt
 sed -i 's/$/api\/json?pretty=true/' $random_folder/queue_item_url.txt
 export queue_item_url=`cat $random_folder/queue_item_url.txt`
 
+export CRUMB=$(curl --user admin:76a4a60136ff3f563f7ad5c3fd52552d http://$JENKINS_SERVER/crumbIssuer/api/xml?xpath=concat\(//crumbRequestField,%22:%22,//crumb\))
+
 # get queue_item_json
 curl -H "$CRUMB" \
      --user admin:76a4a60136ff3f563f7ad5c3fd52552d \
