92087f730ae649df0c31789f03a30da6b8bb052c
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-03-21 15:15:04 -0400

    commit

--- gcloud-scripts/create-epic-environment.sh
+++ gcloud-scripts/create-epic-environment.sh
@@ -23,8 +23,14 @@ fi
 arr=(us-east1-b us-east1-c us-east1-d)
 zone=${arr[$(( ( RANDOM % 3 ) ))]}
 
-# create_vm $1 mysql 2 8
-# create_vm $1 hybris 4 8
-# create_vm $1 solr 4 8
-# create_vm $1 aem 2 8
-# create_vm $1 apache 2 8
+create_vm $1 mysql 2 8
+create_vm $1 hybris 4 8
+create_vm $1 solr 4 8
+create_vm $1 aem 2 8
+
+export epic_name=$1
+apache_ip=`gcloud --format="value(networkInterfaces[0].accessConfigs[0].natIP)" compute instances list --regexp=$epic_name-apache`
+# if apache_ip is empty, create_vm
+if [[ -z $apache_ip ]]; then
+  create_vm $1 apache 2 8
+fi
