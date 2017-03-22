c89e60dd6a8e40fd882d72d3558ea92c03748553
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-03-22 15:50:10 -0400

    commit

--- provision-scripts/aem/provision.sh
+++ provision-scripts/aem/provision.sh
@@ -7,6 +7,7 @@ export SSH_ARGS='-o StrictHostKeyChecking=no'
 export INSTALL_REPOSITORY=/bamboo/data/thdutil/serversetup/Aem
 export DESTINATION_HOST=$1
 
+scp $SSH_ARGS ip_tables_setup.sh root@$DESTINATION_HOST:/root/.
 scp $SSH_ARGS aem_setup_base.sh root@$DESTINATION_HOST:/root/.
 scp $SSH_ARGS aem_setup_env.sh root@$DESTINATION_HOST:/root/.
 scp $SSH_ARGS aem_setup_publish.sh root@$DESTINATION_HOST:/root/.
@@ -14,6 +15,7 @@ ssh $SSH_ARGS root@$DESTINATION_HOST chmod u+x /root/*.sh
 
 scp $SSH_ARGS $INSTALL_REPOSITORY/aem.tar.gz root@$DESTINATION_HOST:~/.
 
+ssh $SSH_ARGS root@$DESTINATION_HOST /root/ip_tables_setup.sh
 ssh $SSH_ARGS root@$DESTINATION_HOST /root/aem_setup_base.sh
 ssh $SSH_ARGS root@$DESTINATION_HOST /root/aem_setup_env.sh
 ssh $SSH_ARGS root@$DESTINATION_HOST /root/aem_setup_publish.sh
