14202636a39b8b84b54f96891daba76780ec1a4b
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-03-22 14:43:51 -0400

    commit

--- provision-scripts/apache/provision.sh
+++ provision-scripts/apache/provision.sh
@@ -11,6 +11,12 @@ scp $SSH_ARGS apache_setup_base.sh root@$DESTINATION_HOST:/root/.
 scp $SSH_ARGS apache_setup_on_docker.sh root@$DESTINATION_HOST:/root/.
 ssh $SSH_ARGS root@$DESTINATION_HOST chmod u+x /root/*.sh
 
-scp $SSH_ARGS $INSTALL_REPOSITORY/*.gz root@$DESTINATION_HOST:~/.
+scp $SSH_ARGS $INSTALL_REPOSITORY/*.gz root@$DESTINATION_HOST:/root/.
+
+# stage a pre built deployment artifact from cloud storage to DESTINATION_HOST
+# gsutil rsync -d -r gs://np-cadotcom.appspot.com/ci-builds/epic-builds/ /tmp/epic-builds
+# export epic_name=${DESTINATION_HOST%-*}
+# export ARTIFACT_LOCATION=/tmp/epic-builds/$epic_name/apache
+# scp $SSH_ARGS $ARTIFACT_LOCATION/*.gz root@$DESTINATION_HOST:/root/.
 
 ssh $SSH_ARGS root@$DESTINATION_HOST /root/apache_setup_base.sh
