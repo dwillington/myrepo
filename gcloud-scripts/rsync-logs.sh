3cc98d9e57a8db29113d8141b52016f3fca12791
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-03-22 21:12:53 -0400

    commit

--- gcloud-scripts/rsync-logs.sh
+++ gcloud-scripts/rsync-logs.sh
@@ -2,9 +2,15 @@
 
 rsync_logs()
 {
-  mkdir -p $4 && rsync -chavzP --delete --stats root@$1-$2:$3 $4
+  ssh root@log-server "mkdir -p $4"
+  ssh root@log-server "rsync -chavzP --delete --stats root@$1-$2:$3 $4"
 }
 
-rsync_logs epic1 solr /opt/solr/homedepot-solr/server/logs /root/log-server/epic1-solr
-rsync_logs epic1 aem /opt/adobe/publish/crx-quickstart/logs/ /root/log-server/epic1-aem
-rsync_logs epic1 hybris /opt/hybris/hybris/log/tomcat/ /root/log-server/epic1-hybris
+if [ $# -lt 1 ]; then
+  echo 1>&2 "$0: missing epic_name"
+  exit 2
+fi
+
+rsync_logs $1 solr /opt/solr/homedepot-solr/server/logs root@log-server:/root/log-server/$1-solr
+rsync_logs $1 aem /opt/adobe/publish/crx-quickstart/logs/ root@log-server:/root/log-server/$1-aem
+rsync_logs $1 hybris /opt/hybris/hybris/log/tomcat/ root@log-server:/root/log-server/$1-hybris
