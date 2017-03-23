5baa68f46ca1aae5d33612affe89fe6438160c11
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-03-22 21:32:31 -0400

    commit

--- gcloud-scripts/rsync-logs-local.sh
+++ gcloud-scripts/rsync-logs-local.sh
@@ -3,7 +3,7 @@
 rsync_logs()
 {
   mkdir -p $4
-  ssh $SSH_ARGS root@log-server "rsync -chavzeP \"ssh -o StrictHostKeyChecking=no\" --delete --stats root@$1-$2:$3 $4"
+  rsync -chavzeP "ssh -o StrictHostKeyChecking=no" --delete --stats root@$1-$2:$3 $4
 }
 
 if [ $# -lt 1 ]; then
