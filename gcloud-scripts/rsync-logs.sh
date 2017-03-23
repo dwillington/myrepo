01d0cdc8958fdb86607b7e104b6bd6a3c626e156
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-03-22 21:43:06 -0400

    commit

--- gcloud-scripts/rsync-logs.sh
+++ gcloud-scripts/rsync-logs.sh
@@ -8,8 +8,3 @@ fi
 SSH_ARGS='-o StrictHostKeyChecking=no'
 scp $SSH_ARGS gcloud-scripts/rsync-logs-local.sh root@log-server:/root/rsync-logs-local.sh
 ssh $SSH_ARGS root@log-server "/root/rsync-logs-local.sh $1"
-
-# ssh $SSH_ARGS root@log-server "mkdir -p /root/log-server/epic1-solr"
-# ssh $SSH_ARGS root@log-server "mkdir -p /root/log-server/epic1-solr rsync -chavzPe \"ssh -o StrictHostKeyChecking=no\" --delete --stats root@epic1-solr:/opt/solr/homedepot-solr/server/logs /root/log-server/epic1-solr"
-
-
