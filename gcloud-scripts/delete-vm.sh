b2081407b8b8b5f3fec2d149ef660a58979fb520
Ashraf <AMJAD_ASHRAF@homedepot.com>
2017-03-22 22:25:48 -0400

    commit

--- gcloud-scripts/delete-vm.sh
+++ gcloud-scripts/delete-vm.sh
@@ -14,7 +14,7 @@ zone=`gcloud --format="value(zone)" compute instances list --regexp=$1-$2`
 
 if [ "$2" == "apache" ]; then
   # ./gcloud-scripts/tear-down-apache.sh $epic_name-apache
-  echo $1-apache
+  echo apache
 else
   # delete_vm $1 $2
   echo $1-$2
