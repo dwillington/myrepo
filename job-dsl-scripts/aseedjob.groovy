c8c25815d5f12d47a25c6b7364c77b66a882a5b1
dwillington <dwillington@yahoo.com>
2017-06-14 15:06:59 -0400

    commit

--- job-dsl-scripts/aseedjob.groovy
+++ job-dsl-scripts/aseedjob.groovy
@@ -1,16 +1,13 @@
 job('AssetMgr') {
-    scm {
-        rtc {
-            configure { node ->
-                // node represents <com.ibm.team.build.internal.hjplugin.RTCScm>
-				buildTypeStr 'buildStream'
-				buildStream 'AssetMgr_Dev'
-            }
-            buildDefinition('AssetMgr_PL_Jenkins_BD_DEV')
+    configure { project ->
+        project / scm(class: 'com.ibm.team.build.internal.hjplugin.RTCScm') {
+            overrideGlobal 'false'
+            buildTypeStr 'buildStream'
+            buildStream 'AssetMgr_Dev'
         }
     }
     steps {
-		maven {
+        maven {
             goals('package')
             localRepository(LocalRepositoryLocation.LOCAL_TO_WORKSPACE)
             mavenInstallation('apache-maven-3.2.1')
