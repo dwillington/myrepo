c533cc4da46c9487eaf81bc79a1f32baf6a4b731
dwillington <dwillington@yahoo.com>
2017-06-19 14:33:16 -0400

    commit

--- job-dsl-scripts/AssetMgr/assetmgr_jobs.groovy
+++ job-dsl-scripts/AssetMgr/assetmgr_jobs.groovy
@@ -1,17 +1,17 @@
 rtc_project_name = 'AssetMgr'
 rtc_stream_name = "${rtc_project_name}_Dev"
 
-job("{rtc_project_name}") {
+job("${rtc_project_name}") {
     configure { project ->
         project / scm(class: 'com.ibm.team.build.internal.hjplugin.RTCScm') {
             overrideGlobal 'false'
             buildTypeStr 'buildStream'
-            buildStream "{rtc_project_name}"
+            buildStream "${rtc_project_name}"
         }
     }
     steps {
         maven {
-            rootPOM("{rtc_project_name}/pom.xml")
+            rootPOM("${rtc_project_name}/pom.xml")
             goals('--batch-mode package')
             mavenInstallation('apache-maven-3.2.1')
             providedGlobalSettings('maven-settings.xml')
