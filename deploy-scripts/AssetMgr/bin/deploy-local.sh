d4af328956f1d15fb47097be12d118a77188a61c
dwillington <dwillington@yahoo.com>
2017-06-22 15:41:47 -0400

    commit

---
+++ deploy-scripts/AssetMgr/bin/deploy-local.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.131-3.b12.el7_3.x86_64/jre
+application_name=AssetMgr
+application_server_name=$(printf "%s%s" $application_name 'Srv')
+cd /tcserver/dev/instance1
+/apps/tcserver/tcruntime-instance.sh create $application_server_name
+mv /tmp/$application_name.war /tcserver/dev/instance1/$application_server_name/webapps/.
+/apps/tcserver/tcruntime-ctl.sh $application_server_name stop
+/apps/tcserver/tcruntime-ctl.sh $application_server_name start
--- deploy-scripts/AssetMgr/bin/deploy.sh
+++ deploy-scripts/AssetMgr/bin/deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.131-3.b12.el7_3.x86_64/jre
-application_name=AssetMgr
-application_server_name=$(printf "%s%s" $application_name 'Srv')
-cd /tcserver/dev/instance1
-/apps/tcserver/tcruntime-instance.sh create $application_server_name
-mv /tmp/$application_name.war /tcserver/dev/instance1/$application_server_name/webapps/.
-/apps/tcserver/tcruntime-ctl.sh $application_server_name stop
-/apps/tcserver/tcruntime-ctl.sh $application_server_name start
+cid=$(sudo docker ps -a | grep 9080 | sed 's/|/ /' | awk '{print $1}')
+docker cp AssetMgr/target/AssetMgr.war $cid:/tmp/.
+docker cp deploy-scripts/AssetMgr/bin/deploy.sh $cid:/tmp/.
+docker exec $cid /tmp/deploy.sh
