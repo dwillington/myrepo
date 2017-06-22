ef9aef65468087bcd042b1f9e919bd26f85c2f29
dwillington <dwillington@yahoo.com>
2017-06-22 15:54:03 -0400

    commit

--- deploy-scripts/AssetMgr/bin/deploy-local.sh
+++ deploy-scripts/AssetMgr/bin/deploy-local.sh
@@ -4,7 +4,11 @@ export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.131-3.b12.el7_3.x86_64/jr
 application_name=AssetMgr
 application_server_name=$(printf "%s%s" $application_name 'Srv')
 cd /tcserver/dev/instance1
-/apps/tcserver/tcruntime-instance.sh create $application_server_name
+
+if [ ! -d "/tcserver/dev/instance1/$application_server_name" ]; then
+  /apps/tcserver/tcruntime-instance.sh create $application_server_name
+fi
+
 mv /tmp/$application_name.war /tcserver/dev/instance1/$application_server_name/webapps/.
 /apps/tcserver/tcruntime-ctl.sh $application_server_name stop
 /apps/tcserver/tcruntime-ctl.sh $application_server_name start
