be21e3f16e66567a8b744c348d6c6690147651ca
dwillington <dwillington@yahoo.com>
2017-06-22 15:45:06 -0400

    commit

--- deploy-scripts/AssetMgr/bin/deploy.sh
+++ deploy-scripts/AssetMgr/bin/deploy.sh
@@ -2,5 +2,5 @@
 
 cid=$(docker ps -a | grep 9080 | sed 's/|/ /' | awk '{print $1}')
 docker cp AssetMgr/target/AssetMgr.war $cid:/tmp/.
-docker cp deploy-scripts/AssetMgr/bin/deploy.sh $cid:/tmp/.
-docker exec $cid /tmp/deploy.sh
+docker cp deploy-scripts/AssetMgr/bin/deploy-local.sh $cid:/tmp/.
+docker exec $cid /tmp/deploy-local.sh
