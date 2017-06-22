d9bf46501b3ab813d8d597f597736ea90b782160
dwillington <dwillington@yahoo.com>
2017-06-22 15:49:04 -0400

    commit

--- deploy-scripts/AssetMgr/bin/deploy.sh
+++ deploy-scripts/AssetMgr/bin/deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-cid=$(docker ps -a | grep 9080 | sed 's/|/ /' | awk '{print $1}')
-docker cp AssetMgr/target/AssetMgr.war $cid:/tmp/.
-docker cp deploy-scripts/AssetMgr/bin/deploy-local.sh $cid:/tmp/.
+if [ $# -lt 1 ] 
+then
+    echo "deploy.sh port"
+    exit 1
+fi
+
+port=$1
+application_name=AssetMgr
+cid=$(docker ps -a | grep $port | sed 's/|/ /' | awk '{print $1}')
+docker cp $application_name/target/$application_name.war $cid:/tmp/.
+docker cp deploy-scripts/$application_name/bin/deploy-local.sh $cid:/tmp/.
 docker exec $cid /tmp/deploy-local.sh
