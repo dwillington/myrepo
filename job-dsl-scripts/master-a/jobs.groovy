22c908b6b9293b2479bb61dd408eaf057ae0c40a
dwillington <dwillington@yahoo.com>
2018-05-16 17:58:09 -0400

    commit

--- job-dsl-scripts/master-a/jobs.groovy
+++ job-dsl-scripts/master-a/jobs.groovy
@@ -1,7 +1,7 @@
 //println System.getProperty("PWD")
 println "${new File(__FILE__).parent}"
 
-evaluate(new File("job-dsl-scripts/tools.groovy"))
+evaluate(new File("${new File(__FILE__).parent}/../../tools.groovy"))
 
 def jobs = [
 	["java-hello-world-with-maven", "https://github.com/jabedhasan21/java-hello-world-with-maven"]
