b136792cc197bff094090374fb62ae07c6888c48
dwillington <dwillington@yahoo.com>
2018-05-16 17:35:40 -0400

    commit

--- job-dsl-scripts/master-b/jobs.groovy
+++ job-dsl-scripts/master-b/jobs.groovy
@@ -4,14 +4,14 @@ def jobs = [
 
 for(int i=0; i<jobs.size(); i++)
 {
-	pipelineJob($jobs[i][0]) {
+	pipelineJob(jobs[i][0]) {
 		definition {
 			cps {
 				sandbox()
 				script("""
 mavenPipeline {
 	branch = "master"
-	scmUrl = $jobs[i][1]
+	scmUrl = jobs[i][1]
 }""".stripIndent())
 			}
 		}
