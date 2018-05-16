466500de12b456d35c13d62d143e4771adaed025
dwillington <dwillington@yahoo.com>
2018-05-16 18:01:52 -0400

    commit

--- job-dsl-scripts/master-a/jobs.groovy
+++ job-dsl-scripts/master-a/jobs.groovy
@@ -8,3 +8,10 @@ def jobs = [
 	]
 
 createPipelineJobs(jobs)
+
+def createPipelineJobs(jobs) { 
+	for(int i=0; i<jobs.size(); i++)
+	{
+		println jobs[i][0]
+	}
+}
--- job-dsl-scripts/tools.groovy
+++
@@ -1,6 +0,0 @@
-def createPipelineJobs(jobs) { 
-	for(int i=0; i<jobs.size(); i++)
-	{
-		println jobs[i][0]
-	}
-}
