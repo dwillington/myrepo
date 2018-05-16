ba0533e0a7442538803063a3e4716fa2382afab6
dwillington <dwillington@yahoo.com>
2018-05-15 23:20:10 -0400

    commit

--- job-dsl-scripts/jobs.groovy
+++ job-dsl-scripts/jobs.groovy
@@ -1,17 +1,13 @@
-job("java-hello-world-maven") {
-    jdk ('jdk-10.0.1')
-    scm {
-        git {
-            branch('master')
-            remote {
-                url("https://github.com/dantheman213/java-hello-world-maven.git")
-            }
-        }
-    }
-    steps {
-        maven {
-            goals('--batch-mode -Dmaven.test.skip=true clean package')
-            mavenInstallation('mvn-3.5.3')
-        }
-    }
+pipelineJob("java-hello-world-maven") {
+    definition {
+        cps {
+			sandbox()
+			script("""
+mavenPipeline {
+	branch = "master"
+	url = "https://github.com/dantheman213/java-hello-world-maven.git"
+}""".stripIndent())
+		}
+	}
 }
+
