eef3adc2c1d7527e7c0e4c4a1e36e421d26fae5f
dwillington <dwillington@yahoo.com>
2018-05-16 17:43:36 -0400

    commit

--- job-dsl-scripts/master-a/jobs.groovy
+++ job-dsl-scripts/master-a/jobs.groovy
@@ -1,4 +1,4 @@
-evaluate(new File("../tools.groovy"))
+evaluate(new File("job-dsl-scripts/tools.groovy"))
 
 def jobs = [
 	["java-hello-world-with-maven", "https://github.com/jabedhasan21/java-hello-world-with-maven"]
