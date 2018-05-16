8cd44f52982e1256223372ee5e6ba1137b1443e2
dwillington <dwillington@yahoo.com>
2018-05-16 18:25:17 -0400

    commit

--- job-dsl-scripts/master-a/jobs.groovy
+++ job-dsl-scripts/master-a/jobs.groovy
@@ -4,4 +4,4 @@ def jobs = [
 	["java-hello-world-with-maven", "https://github.com/jabedhasan21/java-hello-world-with-maven"]
 	]
 
-JobUtils.createPipelineJobs(jobs)
+JobUtils.createPipelineJobs(this, jobs)
