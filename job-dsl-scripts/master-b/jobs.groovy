3affc9853bab011658f363f79fbbad4a3d5abbf3
dwillington <dwillington@yahoo.com>
2018-05-16 17:28:16 -0400

    commit

--- job-dsl-scripts/master-b/jobs.groovy
+++ job-dsl-scripts/master-b/jobs.groovy
@@ -1,5 +1,5 @@
 def jobs = {
-	{spring3-mvc-maven-xml-hello-world", "https://github.com/mkyong/spring3-mvc-maven-xml-hello-world"}
+	{"spring3-mvc-maven-xml-hello-world", "https://github.com/mkyong/spring3-mvc-maven-xml-hello-world"}
 	}
 
 for(int i=0; i<jobs.length; i++)
