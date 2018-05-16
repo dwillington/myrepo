905d533c3db90ba043604b37d20551892487e283
dwillington <dwillington@yahoo.com>
2018-05-16 11:01:56 -0400

    commit

--- vars/mavenPipeline.groovy
+++ vars/mavenPipeline.groovy
@@ -6,7 +6,7 @@ def call(body) {
     body()
 
     pipeline {
-        agent { label 'docker-agent' }
+        agent { label 'master' } //docker-agent
         stages {
             stage('checkout git') {
                 steps {
