10e38bbe18488d3aae2e6a37a760d58498e21a97
dwillington <dwillington@yahoo.com>
2018-05-16 18:03:49 -0400

    commit

--- job-dsl-scripts/master-a/jobs.groovy
+++ job-dsl-scripts/master-a/jobs.groovy
@@ -13,5 +13,22 @@ def createPipelineJobs(jobs) {
 	for(int i=0; i<jobs.size(); i++)
 	{
 		println jobs[i][0]
+		pipelineJob(jobs[i][0]) {
+			definition {
+				cps {
+					sandbox()
+					script("""
+mavenPipeline {
+	branch = "master"
+	scmUrl = jobs[i][1]
+}""".stripIndent())
+				}
+			}
+			publishers {
+				logRotator {
+					numToKeep(5)
+				}
+			}
+		}
 	}
 }
