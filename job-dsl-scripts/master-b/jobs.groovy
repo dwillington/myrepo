1b3a90791a7f572d4902ab37db0e5e1170538e9c
dwillington <dwillington@yahoo.com>
2018-05-16 18:26:17 -0400

    commit

--- job-dsl-scripts/master-b/jobs.groovy
+++ job-dsl-scripts/master-b/jobs.groovy
@@ -1,24 +1,7 @@
+import com.JobUtils
+
 def jobs = [
 	["spring3-mvc-maven-xml-hello-world", "https://github.com/mkyong/spring3-mvc-maven-xml-hello-world"]
 	]
 
-for(int i=0; i<jobs.size(); i++)
-{
-	pipelineJob(jobs[i][0]) {
-		definition {
-			cps {
-				sandbox()
-				script("""
-mavenPipeline {
-	branch = "master"
-	scmUrl = jobs[i][1]
-}""".stripIndent())
-			}
-		}
-		publishers {
-			logRotator {
-				numToKeep(5)
-			}
-		}
-	}
-}
+JobUtils.createPipelineJobs(this, jobs)
