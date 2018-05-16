01b94c246400ee049703413d04d0b45fc2f4912c
dwillington <dwillington@yahoo.com>
2018-05-16 17:42:46 -0400

    commit

--- job-dsl-scripts/master-a/jobs.groovy
+++ job-dsl-scripts/master-a/jobs.groovy
@@ -1,24 +1,7 @@
+evaluate(new File("../tools.groovy"))
+
 def jobs = [
 	["java-hello-world-with-maven", "https://github.com/jabedhasan21/java-hello-world-with-maven"]
 	]
 
-for(int i=0; i<jobs.size(); i++)
-{
-	pipelineJob(jobs[i][0]) {
-		definition {
-			cps {
-				sandbox()
-				script("""
-mavenPipeline {
-	branch = "master"
-	scmUrl = jobs[i][1]
-}""".stripIndent())
-			}
-		}
-		publishers {
-			logRotator {
-				numToKeep(5)
-			}
-		}
-	}
-}
+createPipelineJobs(jobs)
