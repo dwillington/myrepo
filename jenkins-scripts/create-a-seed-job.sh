f5e7016dece29c8f078c837450c220c37b58c3ab
Ashraf <Ashraf.Amjad@td.com>
2018-10-16 14:07:07 -0400

    commit

--- jenkins-scripts/create-a-seed-job.sh
+++ jenkins-scripts/create-a-seed-job.sh
@@ -1,4 +1,4 @@
 username=admin
 password=1196942d9836374d0f56331fcf24480c01
-CRUMB=$(curl -u $username:$password -s 'http://localhost:8081/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
-curl -u $username:$password -H "$CRUMB" -s -XPOST 'http://localhost:8081/createItem?name=a-seed-job' --data-binary @config.xml -H "Content-Type:text/xml"
+CRUMB=$(curl -u $username:$password -s 'http://localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
+curl -u $username:$password -H "$CRUMB" -s -XPOST 'http://localhost:8080/createItem?name=a-seed-job' --data-binary @config.xml -H "Content-Type:text/xml"
