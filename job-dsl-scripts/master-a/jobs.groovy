7641a7c3be222747d16986077de61ecce12bb15f
dwillington <dwillington@yahoo.com>
2019-04-16 21:04:20 -0400

    commit

--- job-dsl-scripts/master-a/jobs.groovy
+++ job-dsl-scripts/master-a/jobs.groovy
@@ -4,8 +4,12 @@ folder("hello-world")
 folder("hello-world/DEV")
 folder("hello-world/SIT")
 
+folder("hello-world-war")
+folder("hello-world-war/DEV")
+
 def jobs = [
-	["hello-world/DEV/java-hello-world-with-maven", "https://github.com/LableOrg/java-maven-junit-helloworld"]
+	["hello-world/DEV/java-hello-world-with-maven", "https://github.com/LableOrg/java-maven-junit-helloworld"],
+	["hello-world-war/DEV/hello-world-war", "https://github.com/dwillington/hello-world-war.git"],
 	]
 
 
