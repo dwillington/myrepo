706e662f837f86b1d28b6ea458993f046cc7f39f
dwillington <dwillington@yahoo.com>
2019-04-17 21:54:03 -0400

    commit

--- job-dsl-scripts/master-a/deploy_jobs.groovy
+++ job-dsl-scripts/master-a/deploy_jobs.groovy
@@ -7,10 +7,14 @@ def jobs = [
 pipelineJob('hello-world-war/DEV/hello-world-war-deploy') {
     definition {
         cps {
+			sandbox()
             script("""mavenDeployPipeline {
 	branch = "master"
 	scmUrl = "$jobScmUrl"
 }""".stripIndent()
         }
     }
+	logRotator {
+		numToKeep(5)
+	}
 }
