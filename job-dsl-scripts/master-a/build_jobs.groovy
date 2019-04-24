32e9f3c3a604c0254c0af3bcc78fb9d7070a9997
dwillington <dwillington@yahoo.com>
2019-04-23 23:11:18 -0400

    commit

--- job-dsl-scripts/master-a/build_jobs.groovy
+++ job-dsl-scripts/master-a/build_jobs.groovy
@@ -3,15 +3,15 @@ import com.JobUtils
 folder("devops")
 folder("devops/devops-war")
 folder("devops/devops-war/DEV")
+folder("devops/devops-war/RELEASE")
 
-folder("devops/devops-jar")
-folder("devops/devops-jar/DEV")
-folder("devops/devops-jar/SIT")
-
+folder("devops/devops-common-jar")
+folder("devops/devops-common-jar/DEV")
 
 def maven_jobs = [
-	["devops/devops-jar/DEV/devops-jar-build", "https://github.com/LableOrg/java-maven-junit-helloworld"],
+	["devops/devops-common-jar/DEV/devops-common-jar-build", "https://github.com/LableOrg/java-maven-junit-helloworld"],
 	["devops/devops-war/DEV/devops-war-build", "https://github.com/dwillington/hello-world-war.git"],
+	["devops/devops-war/RELEASE/devops-war-build", "https://github.com/dwillington/hello-world-war.git"],
 	]
 
 JobUtils.createMavenPipelineJobs(this, maven_jobs)
--- job-dsl-scripts/master-a/deploy_jobs.groovy
+++ job-dsl-scripts/master-a/deploy_jobs.groovy
@@ -3,6 +3,7 @@ import com.JobUtils
 
 def maven_deploy_jobs = [
 	["devops/devops-war/DEV/devops-war-deploy", "https://github.com/dwillington/hello-world-war.git"],
+	["devops/devops-war/RELEASE/devops-war-deploy", "https://github.com/dwillington/hello-world-war.git"],
 	]
 
 JobUtils.createMavenDeployPipelineJobs(this, maven_deploy_jobs)
