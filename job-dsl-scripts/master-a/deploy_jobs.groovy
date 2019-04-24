c839142ad37164ef74b4a002308d9c31fbd30fd4
dwillington <dwillington@yahoo.com>
2019-04-23 20:35:00 -0400

    commit

--- job-dsl-scripts/master-a/deploy_jobs.groovy
+++ job-dsl-scripts/master-a/deploy_jobs.groovy
@@ -5,4 +5,4 @@ def maven_deploy_jobs = [
 	["devops-war/DEV/devops-war-build", "https://github.com/dwillington/hello-world-war.git"],
 	]
 
-JobUtils.createMavenPipelineJobs(this, maven_jobs)
+JobUtils.createMavenPipelineJobs(this, maven_deploy_jobs)
