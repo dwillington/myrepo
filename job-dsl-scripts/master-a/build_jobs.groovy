dbc71e805122c58ecf829edbdd6bca21cdf74b82
dwillington <dwillington@yahoo.com>
2019-04-25 10:07:30 -0400

    commit

--- job-dsl-scripts/master-a/build_jobs.groovy
+++ job-dsl-scripts/master-a/build_jobs.groovy
@@ -9,19 +9,19 @@ folder("devops-project/devops-common-jar-repo")
 folder("devops-project/devops-common-jar-repo/DEV")
 
 def maven_jobs = [
-    ["devops-project/devops-common-jar-repo/DEV/devops-common-jar-build", "https://github.com/LableOrg/java-maven-junit-helloworld"],
-    ["devops-project/devops-war-repo/DEV/devops-war-build", "https://github.com/dwillington/hello-world-war.git"],
-    ["devops-project/devops-war-repo/RELEASE/devops-war-build", "https://github.com/dwillington/hello-world-war.git"],
+    // ["devops-project/devops-common-jar-repo/DEV/devops-common-jar-build", "https://github.com/LableOrg/java-maven-junit-helloworld"],
+    // ["devops-project/devops-war-repo/DEV/devops-war-build", "https://github.com/dwillington/hello-world-war.git"],
+    // ["devops-project/devops-war-repo/RELEASE/devops-war-build", "https://github.com/dwillington/hello-world-war.git"],
     ]
 
 JobUtils.createMavenPipelineJobs(this, maven_jobs)
 
 def project_repo = [
-                     ["doc-project", "doc-repo"],
-                     ["oceanic-project", "oceanic-repo"],
-                     ["atmospheric-project", "atmospheric-repo"],
-                     ["marine-project", "marine-repo"],
-                     ["aviation-project", "aviation-repo"],
+                     // ["doc-project", "doc-repo"],
+                     // ["oceanic-project", "oceanic-repo"],
+                     // ["atmospheric-project", "atmospheric-repo"],
+                     // ["marine-project", "marine-repo"],
+                     // ["aviation-project", "aviation-repo"],
                    ]
 
 for(int i=0; i<project_repo.size(); i++)
