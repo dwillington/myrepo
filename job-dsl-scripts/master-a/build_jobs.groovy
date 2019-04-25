2d49080289c409346aba27f1c608a09c4a17d5ae
dwillington <dwillington@yahoo.com>
2019-04-24 20:42:15 -0400

    commit

--- job-dsl-scripts/master-a/build_jobs.groovy
+++ job-dsl-scripts/master-a/build_jobs.groovy
@@ -1,17 +1,17 @@
 import com.JobUtils
 
-folder("devops")
-folder("devops/devops-war")
-folder("devops/devops-war/DEV")
-folder("devops/devops-war/RELEASE")
+folder("devops-project")
+folder("devops-project/devops-war-repo")
+folder("devops-project/devops-war-repo/DEV")
+folder("devops-project/devops-war-repo/RELEASE")
 
-folder("devops/devops-common-jar")
-folder("devops/devops-common-jar/DEV")
+folder("devops-project/devops-common-jar-repo")
+folder("devops-project/devops-common-jar-repo/DEV")
 
 def maven_jobs = [
-	["devops/devops-common-jar/DEV/devops-common-jar-build", "https://github.com/LableOrg/java-maven-junit-helloworld"],
-	["devops/devops-war/DEV/devops-war-build", "https://github.com/dwillington/hello-world-war.git"],
-	["devops/devops-war/RELEASE/devops-war-build", "https://github.com/dwillington/hello-world-war.git"],
+ 	["devops-project/devops-common-jar-repo/DEV/devops-common-jar-build", "https://github.com/LableOrg/java-maven-junit-helloworld"],
+	["devops-project/devops-war-repo/DEV/devops-war-build", "https://github.com/dwillington/hello-world-war.git"],
+	["devops-project/devops-war-repo/RELEASE/devops-war-build", "https://github.com/dwillington/hello-world-war.git"],
 	]
 
 JobUtils.createMavenPipelineJobs(this, maven_jobs)
--- job-dsl-scripts/master-a/deploy_jobs.groovy
+++ job-dsl-scripts/master-a/deploy_jobs.groovy
@@ -2,8 +2,8 @@ import com.JobUtils
 
 
 def maven_deploy_jobs = [
-	["devops/devops-war/DEV/devops-war-deploy", "https://github.com/dwillington/hello-world-war.git"],
-	["devops/devops-war/RELEASE/devops-war-deploy", "https://github.com/dwillington/hello-world-war.git"],
+	["devops-project/devops-war-repo/DEV/devops-war-deploy", "https://github.com/dwillington/hello-world-war.git"],
+	["devops-project/devops-war-repo/RELEASE/devops-war-deploy", "https://github.com/dwillington/hello-world-war.git"],
 	]
 
 JobUtils.createMavenDeployPipelineJobs(this, maven_deploy_jobs)
