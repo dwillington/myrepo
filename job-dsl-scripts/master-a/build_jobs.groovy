2db7311718ddd9c076c8f179b345aa7a849267a2
dwillington <dwillington@yahoo.com>
2019-04-25 10:08:08 -0400

    commit

--- job-dsl-scripts/master-a/build_jobs.groovy
+++ job-dsl-scripts/master-a/build_jobs.groovy
@@ -1,12 +1,12 @@
 import com.JobUtils
 
-folder("devops-project")
-folder("devops-project/devops-war-repo")
-folder("devops-project/devops-war-repo/DEV")
-folder("devops-project/devops-war-repo/RELEASE")
+// folder("devops-project")
+// folder("devops-project/devops-war-repo")
+// folder("devops-project/devops-war-repo/DEV")
+// folder("devops-project/devops-war-repo/RELEASE")
 
-folder("devops-project/devops-common-jar-repo")
-folder("devops-project/devops-common-jar-repo/DEV")
+// folder("devops-project/devops-common-jar-repo")
+// folder("devops-project/devops-common-jar-repo/DEV")
 
 def maven_jobs = [
     // ["devops-project/devops-common-jar-repo/DEV/devops-common-jar-build", "https://github.com/LableOrg/java-maven-junit-helloworld"],
