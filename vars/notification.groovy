aa30a7ab52cdb822f37970e687fbe0e24e8655ed
Ashraf <aashraf@corp.transunion.com>
2019-06-13 19:20:41 -0400

    commit

--- vars/notification.groovy
+++ vars/notification.groovy
@@ -26,6 +26,8 @@ def call(config) {
 				def scmNotification = new com.td.jenkins.notify.scm.SCMNotification(this, config.stage_name)
 				currentBuild.result = scmNotification.run(config)
 				break
+			case "PUBLISH_REPORT":
+				break
 			default:
 				com.td.jenkins.util.Utilities.printToConsoleOutput(this, ["RED", "notification_type cannot be found"])
 				currentBuild.result = "FAILURE"
