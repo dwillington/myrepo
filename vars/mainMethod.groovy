fb3174139dfaefeed55a07c7b1eee4d25c995468
Ashraf <aashraf@corp.transunion.com>
2019-06-13 16:05:39 -0400

    commit

--- vars/mainMethod.groovy
+++ vars/mainMethod.groovy
@@ -3,7 +3,6 @@ import groovy.json.*
 def call(body) {
 		Map config = null // stores entire job configuration
 
-		properties([parameters([string(name: 'REPO_URL'])]))
 		echo params.REPO_URL
 
 		String scmName = params.REPO_URL.split("/")[scmUrl.split("/").size()-1].replace(".git", "")
