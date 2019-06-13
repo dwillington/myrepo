cce3ce203627dd14a97dc1604f151bac0a097e04
Ashraf <aashraf@corp.transunion.com>
2019-06-13 16:41:57 -0400

    commit

--- job-dsl-scripts/blxblddev2/jobs.groovy
+++ job-dsl-scripts/blxblddev2/jobs.groovy
@@ -3,13 +3,15 @@ folder("CIAD-TEST/hello-world-war")
 folder("CIAD-TEST/hello-world-war/DEV")
 
 pipelineJob('CIAD-TEST/hello-world-war/DEV/hello-world-war-pipeline') {
-	parameters {
-        stringParam('REPO_URL', 'https://github.com/dwillington/hello-world-war.git')
-        stringParam('BRANCH', 'master')
-    }
+	// parameters {
+        // stringParam('REPO_URL', 'https://github.com/dwillington/hello-world-war.git')
+        // stringParam('BRANCH', 'master')
+    // }
     definition {
         cps {
             script("""@Library('myrepo') _
+    scmUrl = "https://github.com/dwillington/hello-world-war.git"
+    branch = "master"
 mainMethod()"""
 			)
             sandbox()
--- vars/mainMethod.groovy
+++ vars/mainMethod.groovy
@@ -1,12 +1,20 @@
 import groovy.json.*
 
 def call(body) {
+		// evaluate the body block, and collect configuration into the object
+		def pipelineParams= [:]
+		body.resolveStrategy = Closure.DELEGATE_FIRST
+		body.delegate = pipelineParams
+		body()
+
 		Map config = null // stores entire job configuration
 
-		String scmUrl = params.REPO_URL
-		echo scmUrl
+		// String scmUrl = params.REPO_URL
+		// String scmName = scmUrl.split("/")[scmUrl.split("/").size()-1].replace(".git", "")		
+		// echo scmName
 
-		String scmName = scmUrl.split("/")[scmUrl.split("/").size()-1].replace(".git", "")		
-		echo scmName
+		echo scmUrl
+		echo branch
+
 
 }
