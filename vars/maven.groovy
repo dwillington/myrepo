3c458ebafb38fdcf56fea8fe7e2f764c7902a7e0
Darcy Willington <dwillington@yahoo.com>
2019-06-13 17:18:42 -0400

    commit

---
+++ vars/maven.groovy
@@ -0,0 +1,38 @@
+def call(config) {
+	def required = ["agent", "stage_name", "checkout_scm", "clean_up_workspace", "maven_operation_type", "maven_command"]
+	switch(config.maven_operation_type) {
+		case "BUILD":
+			break
+		case "PUBLISH":
+			required.add("version_strategy")
+			break
+		case "RELEASE":
+			required.add("version_strategy")
+			break
+		default:
+			break
+	}
+
+	if (com.td.jenkins.util.Utilities.hasRequiredConfig(this, config, required)) {
+		switch(config.maven_operation_type) {
+			case "BUILD":
+				def mavenBuild = new com.td.jenkins.build.java.MavenBuild(this, config.stage_name)
+				currentBuild.result = mavenBuild.run(config)
+				break
+			case "PUBLISH":
+				def mavenPublish = new com.td.jenkins.publish.java.MavenPublish(this, config.stage_name)
+				currentBuild.result = mavenPublish.run(config)
+				break
+			case "RELEASE":
+				def mavenRelease = new com.td.jenkins.publish.java.MavenRelease(this, config.stage_name)
+				currentBuild.result = mavenRelease.run(config)
+				break
+			default:
+				com.td.jenkins.util.Utilities.printToConsoleOutput(this, ["RED", "maven_operation_type cannot be found"])
+				currentBuild.result = "FAILURE"
+		}
+	}
+	else {
+		currentBuild.result = "FAILURE"
+	}
+}
---
+++ vars/notification.groovy
@@ -0,0 +1,42 @@
+def call(config) {
+	def required = ["agent", "stage_name", "checkout_scm", "clean_up_workspace", "notification_type"]
+	switch(config.notification_type) {
+		case "EMAIL":
+			required.add("notification_email_sender")
+			required.add("notification_email_recipients")
+			// required.add("email_type")
+			break
+		case "SCM":
+			required.add("notification_scm_account_id")
+			required.add("in_progress")
+			break
+		case "PUBLISH_REPORT":
+			break
+		default:
+			break
+	}
+
+	if (com.td.jenkins.util.Utilities.hasRequiredConfig(this, config, required)) {
+		switch(config.notification_type) {
+			case "EMAIL":
+				def emailNotification = new com.td.jenkins.notify.email.EmailNotification(this, config.stage_name)
+				currentBuild.result = emailNotification.run(config)
+				break
+			case "SCM":
+				def scmNotification = new com.td.jenkins.notify.scm.SCMNotification(this, config.stage_name)
+				currentBuild.result = scmNotification.run(config)
+				break
+			case "PUBLISH_REPORT":
+				def publishReport = new com.td.jenkins.util.PublishReport(this, config.stage_name)
+				currentBuild.result = publishReport.run(config)
+				break
+			default:
+				com.td.jenkins.util.Utilities.printToConsoleOutput(this, ["RED", "notification_type cannot be found"])
+				currentBuild.result = "FAILURE"
+				break
+		}
+	}
+	else {
+		currentBuild.result = "FAILURE"
+	}
+}
