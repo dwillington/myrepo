46ea4278554a8b00ffb7b84c927a1477937e48b6
Ashraf <aashraf@corp.transunion.com>
2019-06-13 15:55:46 -0400

    commit

--- job-dsl-scripts/blxblddev2/jobs.groovy
+++ job-dsl-scripts/blxblddev2/jobs.groovy
@@ -4,7 +4,7 @@ folder("CIAD-TEST/hello-world-war/DEV")
 
 pipelineJob('CIAD-TEST/hello-world-war/DEV/hello-world-war-pipeline') {
 	parameters {
-        stringParam('REPO_URL', 'https://github.com/dwillington/hello-world-war.git')
+        stringParam('scmName', 'https://github.com/dwillington/hello-world-war.git')
     }
     definition {
         cps {
--- vars/mainMethod.groovy
+++ vars/mainMethod.groovy
@@ -2,7 +2,11 @@ import groovy.json.*
 
 def call(body) {
 		Map config = null // stores entire job configuration
-		String scmName = scmUrl.split("/")[scmUrl.split("/").size()-1].replace(".git", "")
+
+		properties([parameters([string(name: 'REPO_URL'])])
+		echo params.REPO_URL
+
+		String scmName = params.REPO_URL.split("/")[scmUrl.split("/").size()-1].replace(".git", "")
 		
 		echo scmName
 
