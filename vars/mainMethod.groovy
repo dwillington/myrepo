a4f448e78bae74c3488fc481818827051cb4a405
Ashraf <aashraf@corp.transunion.com>
2019-06-26 11:26:11 -0400

    commit

--- vars/mainMethod.groovy
+++ vars/mainMethod.groovy
@@ -13,20 +13,21 @@ def call(body) {
 		Map defaultWorkflow = null // stores steps for the default branch
 
 		String scmUrl;
-		String currentBranch; // this will only capture the prefix branch name i.e feature/abc -> currentBranch = feature
+		String branchPrefix; // this will only capture the branch name prefix i.e feature/abc -> branchPrefix = feature
 		String fullBranchName;
 
-		if(env.BRANCH_NAME) {
-		    // scmUrl = this.scm.getUserRemoteConfigs()[0].getUrl()
+		if(env.BRANCH_NAME) { 
+			// MULTI BRANCH PIPELINE, scm url, branch parameters are introspected
 			scmUrl = this.scm.getSource()
 			fullBranchName = env.BRANCH_NAME
 		}
 		else {
+			// REGULAR PIPELINE, scm url, branch parameters are passed in
 			scmUrl = pipelineParams.scmUrl
 			fullBranchName = pipelineParams.branch
 		}
 
-		currentBranch = fullBranchName.split('/')[0]
+		branchPrefix = fullBranchName.split('/')[0]
 
 		// if (env.BRANCH_NAME.split('/').size() > 2) {
 			// com.td.jenkins.util.Utilities.printToConsoleOutput(this, [["RED", "FATAL: Improper branch name, more than one forward slash '/' not allowed"],
@@ -40,7 +41,7 @@ def call(body) {
 		String scmName = scmUrl.split("/")[scmUrl.split("/").size()-1].replace(".git", "")		
 		echo "scmUrl: " + scmUrl
 		echo "fullBranchName: " + fullBranchName
-		echo "currentBranch: " + currentBranch
+		echo "branchPrefix: " + branchPrefix
 		echo "scmName: " + scmName
 
 		// Reading pipeline configuration. This step will be performed by a default agent or master
@@ -61,11 +62,11 @@ def call(body) {
 		// Validating pipeline steps for the current branch
     	currentBuild.result = "SUCCESS"
 	    for (wf in config.workflow) {
-	    	if (wf.branch == currentBranch) {
+	    	if (wf.branch == branchPrefix) {
 	     		workflow = wf
 	     		com.tuc.jenkins.util.Utilities.printToConsoleOutput(this, [
 	     			["GREEN", "Successfully detected configuration for current branch. Executing pipeline on"],
-	     			["WHITE", currentBranch]], " : ")
+	     			["WHITE", branchPrefix]], " : ")
 	     	}
 	     	else if (wf.branch == config.default_branch) {
 	     		defaultWorkflow = wf
@@ -75,7 +76,7 @@ def call(body) {
 			if (defaultWorkflow == null) {
 				com.tuc.jenkins.util.Utilities.printToConsoleOutput(this, [
 					["RED", "FATAL: No workflows match the current branch"],
-					["WHITE", currentBranch]], " : ")
+					["WHITE", branchPrefix]], " : ")
 		 		currentBuild.result = "ABORTED"
 		 		return // exit pipeline prematurely
 		 	}
