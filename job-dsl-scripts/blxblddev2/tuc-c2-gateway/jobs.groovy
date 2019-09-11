83801f5b648583801044813f8062e9e106bc1f99
Ashraf <aashraf@corp.transunion.com>
2019-09-11 13:10:45 -0400

    commit

--- job-dsl-scripts/blxblddev2/tuc-c2-gateway/jobs.groovy
+++ job-dsl-scripts/blxblddev2/tuc-c2-gateway/jobs.groovy
@@ -4,17 +4,30 @@ folder("CIAD-TEST/apps/services")
 folder("CIAD-TEST/apps/services/tuc-c2-gateway")
 folder("CIAD-TEST/apps/services/tuc-c2-gateway/DEV")
 
-pipelineJob('CIAD-TEST/apps/services/tuc-c2-gateway/DEV/tuc-c2-gateway-pipeline') {
-    definition {
-        cps {
-            script("""@Library('myrepo') _
-mainMethod{
-    // scmUrl = "https://github.com/dwillington/hello-world-war.git"
-	scmUrl = "http://blxblddev3.transunion.ca:10002/apps/services/tuc-c2-gateway"
-    branch = "default"
-}"""
-			)
-            sandbox()
+// pipelineJob('CIAD-TEST/apps/services/tuc-c2-gateway/DEV/tuc-c2-gateway-pipeline') {
+    // definition {
+        // cps {
+            // script("""@Library('myrepo') _
+// mainMethod{
+	// scmUrl = "http://blxblddev3.transunion.ca:10002/apps/services/tuc-c2-gateway"
+    // branch = "default"
+// }"""
+			// )
+            // sandbox()
+        // }
+    // }
+// }
+
+multibranchPipelineJob('CIAD-TEST/apps/services/tuc-c2-gateway/DEV/tuc-c2-gateway-multibranch-pipeline-dsl') {
+    branchSources {
+        hg ('http://blxblddev3.transunion.ca:10002/apps/services/tuc-c2-gateway') {
+            installation('builtin')
+            credentialsId('jenkins-rhodecode')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(1)
         }
     }
 }
